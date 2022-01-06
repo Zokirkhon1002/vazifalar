8c49f1e1b02839f34e61994f27600ae5aa5aa4bb
Zokirkhon <78657264+Zokirkhon1002@users.noreply.github.com>
2022-01-06 16:48:01 +0900

    11-vazifa

---
+++ 11-vazifa, (gfs)error/app.js
@@ -0,0 +1,163 @@
+const express = require("express");
+const app = express();
+const crypto = require("crypto");
+const path = require("path");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const GridFsStorage = require("multer-gridfs-storage");
+
+// Middleware
+app.use(express.json());
+app.set("view engine", "ejs");
+
+// DB
+const mongoURI = "mongodb://localhost/files";
+
+// connection
+const conn = mongoose.createConnection(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// init gfs
+let gfs;
+conn.once("open", () => {
+  // init stream
+  gfs = new mongoose.mongo.GridFSBucket(conn.db, {
+    bucketName: "uploads"
+  });
+});
+
+// // Storage
+// const storage = new GridFsStorage({
+//   url: mongoURI,
+//   file: (req, file) => {
+//     return new Promise((resolve, reject) => {
+//       crypto.randomBytes(16, (err, buf) => {
+//         if (err) {
+//           return reject(err);
+//         }
+//         const filename = buf.toString("hex") + path.extname(file.originalname);
+//         const fileInfo = {
+//           filename: filename,
+//           bucketName: "uploads"
+//         };
+//         resolve(fileInfo);
+//       });
+//     });
+//   }
+// });
+const storage = new GridFsStorage({ url: mongoURI }) // soddaroq usuli
+
+const upload = multer({
+  storage
+});
+
+// route'lar
+app.get("/", (req, res) => {
+  if (!gfs) {
+    const error = "Kutilmagan xato yuz berdi.";
+    console.log(error);
+    res.send(error);
+    process.exit(0);
+  }
+  gfs.find().toArray((err, files) => {
+    // check if files
+    if (!files || files.length === 0) {
+      return res.render("index", {
+        files: false
+      });
+    } else {
+      const f = files
+        .map(file => {
+          if (
+            file.contentType === "image/png" ||
+            file.contentType === "image/jpeg"
+          ) {
+            file.isImage = true;
+          } else {
+            file.isImage = false;
+          }
+          return file;
+        })
+        .sort((a, b) => {
+          return (
+            new Date(b["uploadDate"]).getTime() -
+            new Date(a["uploadDate"]).getTime()
+          );
+        });
+
+      return res.render("index", {
+        files: f
+      });
+    }
+
+    // return res.json(files);
+  });
+});
+
+app.post("/upload", upload.single("file"), (req, res) => {
+  res.redirect("/");
+});
+
+app.get("/files", (req, res) => {
+  gfs.find().toArray((err, files) => {
+    // fayl mavjudligini tekshiramiz
+    if (!files || files.length === 0) {
+      return res.status(404).json({
+        err: "bironta ham fayl mavjud emas"
+      });
+    }
+
+    return res.json(files);
+  });
+});
+
+app.get("/files/:filename", (req, res) => {
+  gfs.find(
+    {
+      filename: req.params.filename
+    },
+    (err, file) => {
+      if (!file) {
+        return res.status(404).json({
+          err: "bunday fayl mavjud emas"
+        });
+      }
+
+      return res.json(file);
+    }
+  );
+});
+
+app.get("/image/:filename", (req, res) => {
+  const file = gfs
+    .find({
+      filename: req.params.filename
+    })
+    .toArray((err, files) => {
+      if (!files || files.length === 0) {
+        return res.status(404).json({
+          err: "bunday rasm mavjud emas"
+        });
+      }
+      gfs.openDownloadStreamByName(req.params.filename).pipe(res);
+    });
+});
+
+// files/del/:id
+// faylni database'dan o'chiramiz
+app.post("/files/del/:id", (req, res) => {
+  gfs.delete(new mongoose.Types.ObjectId(req.params.id), (err, data) => {
+    if (err)
+      return res.status(404).json({ err: err.message });
+
+    res.redirect("/");
+  });
+});
+
+const port = process.env.PORT || 5001;
+
+app.listen(port, () => {
+  console.log(`${port}chi portni eshitishni boshladim...`);
+});
