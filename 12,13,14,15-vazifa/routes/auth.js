944ca73b191c3b017f532d4088e0e8ce45029766
Zokirkhon <78657264+Zokirkhon1002@users.noreply.github.com>
2022-01-09 04:10:29 +0900

    12,13,14,15 - vazifalar

---
+++ 12,13,14,15-vazifa/routes/auth.js
@@ -0,0 +1,44 @@
+const { User } = require("../models/user");
+const express = require("express");
+const userRouter = express.Router();
+const bcrypt = require("bcrypt");
+const Joi = require("joi");
+const _ = require("lodash");
+
+userRouter.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+
+  if (!user) return res.status(400).send("Email yoki parol noto'g'ri");
+
+  const isValidPassword = await bcrypt.compare(
+    req.body.password,
+    user.password
+  );
+
+  if (!isValidPassword) {
+    return res.status(400).send("Email yoki parol noto'g'ri");
+  }
+
+  res.send(true)
+  //   user = new User(_.pick(req.body, ["name", "email", "password"]));
+
+  //   // parolni hashlash
+  //   const salt = await bcrypt.genSalt();
+  //   user.password = await bcrypt.hash(user.password, salt);
+
+  //   await user.save();
+  //   res.send(_.pick(user, ["_id", "name", "email"]));
+});
+
+function validate(req) {
+  const schema = {
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required(),
+  };
+  return Joi.validate(req, schema);
+}
+
+module.exports = userRouter;
