e08294e30c554f305bed3f6d6c38638d5ffbf9d5
Zokirkhon <78657264+Zokirkhon1002@users.noreply.github.com>
2022-03-09 22:40:06 +0900

    18 va 19 darslar yuklandi

---
+++ 18,19-vazifalar/models/user.js
@@ -0,0 +1,41 @@
+const Joi = require("joi");
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 1024,
+  },
+  isAdmin: Boolean
+});
+//
+
+const User = mongoose.model("User", userSchema);
+
+function validateUser(user){
+    const schema = {
+        name: Joi.string().min(3).max(50).required(),
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(255).required(),
+        isAdmin: Joi.boolean().required(),
+    }
+    return Joi.validate(user,schema)
+}
+
+exports.User = User;
+exports.validate = validateUser;
